c3d8030686923ab10a434ee9cc2fb3a7b1846fac
Potential <michael.rothammer@gmail.com>
2018-10-28 00:52:22 +0200

    debug stuff

--- classes/dataScripts.php
+++ classes/dataScripts.php
@@ -56,11 +56,14 @@ class dataScripts
         require_once("../classes/timeScripts.php");
         $timesHandle = new timeScripts();
 
+        header("Location: debug.php?info="."Working: requiring class");
+
         $timesInfo = $timesHandle->getDefaultTimes($schedule);
         $oldInfo = $this->getOldScheduleTimes($_SESSION["userName"], $schedule);
 
         if($oldInfo !== null && count($oldInfo[0]) !== 0){
             $timesInfo = $oldInfo;
+            header("Location: debug.php?info="."Acquiring old info!?");
         }
 
         $starttimes = $timesInfo[0];
@@ -71,9 +74,15 @@ class dataScripts
 
         for($i = 0; $i < $sleepscount; $i++) {
             $inserttimes = "INSERT INTO scheduletimes (scheduleID, starttime, endtime, sleeptype) VALUES (?,?,?,?)";
-            $readyquery = $conn->prepare($inserttimes);
-            $readyquery->bind_param("iiis",$scheduleID, $starttimes[$i], $endtimes[$i], $sleeptypes[$i]);
-            $readyquery->execute();
+            if(!($readyquery = $conn->prepare($inserttimes))){
+                header("Location: debug.php?info=".$conn->error);
+            }
+            if(!($readyquery->bind_param("iiis",$scheduleID, $starttimes[$i], $endtimes[$i], $sleeptypes[$i]))){
+                header("Location: debug.php?info=".$conn->error);
+            }
+            if(!($readyquery->execute())){
+                header("Location: debug.php?info=".$conn->error);
+            }
         }
     }
 
@@ -83,7 +92,7 @@ class dataScripts
         $timesByName = "SELECT st.starttime, st.endtime, st.sleeptype, st.id FROM scheduletimes st
                         INNER JOIN 
                             (SELECT max(s.id) AS id
-                            FROM (schedules s
+                            FROM (Schedules s
                             INNER JOIN
                                 (SELECT * FROM users WHERE username = ?) userdude
                             ON userdude.id = s.userID)) sy
@@ -112,7 +121,7 @@ class dataScripts
         $recentTimes = "SELECT st.starttime, st.endtime, st.sleeptype, sy.starttimestamp FROM scheduletimes st
                         INNER JOIN 
                             (SELECT s.id AS id, starttimestamp
-                            FROM (schedules s
+                            FROM (Schedules s
                             INNER JOIN
                                 (SELECT * FROM users WHERE username = ?) userdude
                             ON userdude.id = s.userID)
---
+++ debug.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>GET AWAY FROM HERE</title>
+    <link rel="icon"
+          type="image/png"
+          href="/resources/images/favicon.png">
+    <link rel="stylesheet" href="/genstyles.css">
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <script src="/scripts.js"></script>
+</head>
+<body>
+
+<?php
+echo $_GET["info"];
